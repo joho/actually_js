0da8d0f1d8e9770f9d6b5fd90cf64ca9e99db0d6
John Barton (joho) <jrbarton@gmail.com>
2017-02-10 11:54:50 +1100

    Actually, post this to twitter.
    https://twitter.com/actually_js/status/829855917164212224

--- actually.rb
+++ actually.rb
@@ -93,7 +93,14 @@ generator = Generator.new(markov)
 #
 # generator.fresh_actuallies(all_actuallies)
 
-p generator.mansplain
+twitter = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['TWITTER_KEY']
+  config.consumer_secret     = ENV['TWITTER_SECRET']
+  config.access_token        = ENV['TWITTER_USER_TOKEN']
+  config.access_token_secret = ENV['TWITTER_USER_SECRET']
+end
 
-binding.pry
+well_actually = generator.mansplain.shuffle[0]
+
+p twitter.update(well_actually)
 
